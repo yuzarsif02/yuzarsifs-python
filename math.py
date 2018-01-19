008f464eedd94688477a8892d4c7835943d8e416
yuzarsif02 <35587643+yuzarsif02@users.noreply.github.com>
2018-01-19 10:36:25 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,8 @@
+a=int(input("enter a value"))
+b=int(input("enter b value "))
+c=a+b
+print(c)
+a=float(input("enter a value"))
+b=float(input("enter b value "))
+c=a+b
+print(c)
---
+++ program.py
@@ -0,0 +1,6 @@
+#my first program
+print("Mohammedyusuf")
+print('its very fun')
+print('its very fun in cmru')
+print('i love playing TT')
+print("200+20")
